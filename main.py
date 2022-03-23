ab4fe9efd6465fd586da7b0203c6e77708f79d12
19-Evgen-86 <95378553+19-Evgen-86@users.noreply.github.com>
2022-03-23 10:59:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 import sqlite3
 
-with sqlite3.connect("animal.db") as db:
-    db.row_factory =sqlite3.Row
-    res = db.execute("select * from animals").fetchall()
 
-    for i in res:
-        print(dict(i))
+def get_db_data(db_name, sql):
+    with sqlite3.connect(db_name) as db:
+        db.row_factory = sqlite3.Row
+        return db.execute(sql).fetchall()
 
 
+def set_db_data(db_name, sql):
+    with sqlite3.connect(db_name) as db:
+        db.execute(sql)
+
+
+def create_table():
+    sql_animals = """ create table `animals` (
+                `id` varchar(30) UNIQUE,
+                `name` NVARCHAR(50),
+                `animal_type` NVARCHAR(30),
+                `breed` NVARCHAR(30),
+                `date_of_birth` DATE,
+                `color_1` NVARCHAR(10),
+                `color_2` NVARCHAR(10)  
+             )"""
+    set_db_data("shelter.db", sql_animals)
+
+    sql_shelter = """ create table `shelter` (
+                    `id` integer PRIMARY KEY AUTOINCREMENT,
+                    `animal_id` VARCHAR(30) UNIQUE,
+                    `outcome_subtype` NVARCHAR(30),
+                    `outcome_type` NVARCHAR(30),
+                    `outcome_month` INTEGER,
+                    `outcome_year` INTEGER,
+                    `age_upon_outcome` NVARCHAR(10),
+
+                    FOREIGN KEY (animal_id) REFERENCES animals(id)
+                 )"""
+    set_db_data("shelter.db", sql_shelter)
+
+
+if __name__ == '__main__':
+    pass
