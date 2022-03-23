a11bfe9e26671ea3b422c9bf94753bfe6829147c
19-Evgen-86 <95378553+19-Evgen-86@users.noreply.github.com>
2022-03-23 16:27:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def set_db_data(db_name, sql):
         db.execute(sql)
 
 
-def create_table():
+def create_table_shelter_db():
     sql_animals = """ create table `animals` (
                 `id` varchar(30) UNIQUE,
                 `name` NVARCHAR(50),
@@ -38,5 +38,21 @@ def create_table():
     set_db_data("shelter.db", sql_shelter)
 
 
+def copy_animals_db_to_shelter_db():
+    with sqlite3.connect("animal.db") as animals, sqlite3.connect("shelter.db") as shelter:
+        animals_data_to_shelter = animals.execute("""select `animal_id`,`outcome_subtype`,`outcome_type`,`outcome_month`,
+                                                  `outcome_year`,`age_upon_outcome`
+                                                  from animals""").fetchall()
+
+        animals_data_to_animals = animals.execute("""select animal_id,name,animal_type,breed,"
+                        "date_of_birth,color1,color2 from animals""").fetchall()
+
+
+        shelter.executemany("insert or ignore into shelter(animal_id,outcome_subtype,outcome_type,outcome_month,"
+                        "outcome_year,age_upon_outcome) values (?,?,?,?,?,?)", animals_data_to_shelter)
+        shelter.executemany("insert or ignore into animals("
+                        "date_of_birth) values (?)", animals_data_to_animals)
+
 if __name__ == '__main__':
-    pass
+    for item in(get_db_data("shelter.db", "select * from animals limit 10")):
+        print(list(item))
